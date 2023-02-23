f16b0e3097a24dad9ee0ef4bbc3a69bfa4ca7894
vladmoroca <vladmoroca@gmail.com>
2023-02-23 20:14:47 +0200

    added interactive mode

--- lab1/main.js
+++ lab1/main.js
@@ -5,12 +5,7 @@ const GetDiscriminant = (a, b, c) => {
 }
 
 const NumToString = num => {
-    if(num > 0) {
-        return num === 1 ? '+' : `+ ${num}` 
-    }
-    if(num < 0) {
-        return num === -1 ? '-' : `${num}` 
-    }
+    return num > 0 ? `+ ${num}` : `${num}`
 } 
 
 const EquationLog = (a, b, c, D) => {
@@ -34,4 +29,43 @@ const QuadraticEquationSolver = (a, b, c) => {
     if(x2 && x2 !== x1) console.log("x2:" + x2)
 }
 
-QuadraticEquationSolver(9, -30, 25)
+const Interactive = () => {
+    const questions = ["a = ", "b = ", "c = "];
+    const params = [];
+  
+    const checkNumber = (data) => {
+      return (
+        data.toString().trim() === parseInt(data.toString()).toString().trim() ||
+        data.toString().trim() === parseFloat(data.toString()).toString().trim()
+      );
+    };
+  
+    process.stdin.on("data", (data) => {
+      if (params.length < questions.length) {
+        const parsedData = parseFloat(data.toString());
+        if (params.length === 0 && parsedData === 0) {
+          console.log("Error. a cannot be 0");
+          process.stdout.write(questions[0]);
+        } else if (!checkNumber(data)) {
+          console.log(
+            `Error. Expected a valid real number, got ${data
+              .toString()
+              .trim()} instead`
+          );
+          process.stdout.write(questions[params.length]);
+        } else {
+          params.push(parsedData);
+          if (params.length < questions.length) {
+            process.stdout.write(questions[params.length]);
+          } else {
+            (QuadraticEquationSolver(...params));
+            process.exit(0);
+          }
+        }
+      }
+    });
+  
+    process.stdout.write(questions[0]);
+  };
+
+Interactive()
